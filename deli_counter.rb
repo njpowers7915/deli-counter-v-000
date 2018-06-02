5f999c3a87a87f529e14d2e231cfa3416bc0b26c
Nicholas Powers <njpowers7915@gmail.com>
2018-06-02 02:45:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    line_array = ["The line is currently:"]
+    katz_deli.each do |person|
+      count = 1
+      x = "#{count}. #{person}"
+      line_array << x
+      count += 1
+    end
+    
+  end
+end
+
+def take_a_number(katz_deli, name)
+  count = 0
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.size}"
+end
+
+def now_serving(katz_deli)
+  
+end
